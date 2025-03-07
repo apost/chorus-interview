8944003e07af46c0e749a143abbdba12bf932057
Andrew Post <andrewpost@gmail.com>
2025-03-07 12:08:07 -0800

    app.tsx changes supporting the routes and views
    This should have been pushed up earlier, to validate successful test progress

--- packages/pokemon-ui/src/app/app.tsx
+++ packages/pokemon-ui/src/app/app.tsx
@@ -1,9 +1,18 @@
 import styled from '@emotion/styled';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import NxWelcome from './nx-welcome';
+import ProfileSelectionView from './profileSelectionView';
+import TeamSelectionView from './teamSelectionView';
 
 export function App() {
-  return <NxWelcome title="pokemon-ui" />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProfileSelectionView />} />
+        <Route path="/team/:team" element={<TeamSelectionView />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
