743a12dd0d7c5694e6cdd09159be3d5d4a4e8f07
Andrew Post <andrewpost@gmail.com>
2025-03-07 17:29:55 -0800

    Re-add pokemon module dependencies
    Re-implement the erroring pokemon service code
    Add the missing ypeOrmModule for the entity into the pokemon module

--- packages/pokemon-user-backend/src/modules/pokemon/pokemon.module.ts
+++ packages/pokemon-user-backend/src/modules/pokemon/pokemon.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonController } from './pokemon.controller';
 import { PokemonService } from './pokemon.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PokemonPrototype } from '@entities/PokemonPrototype';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PokemonPrototype])],
   controllers: [PokemonController],
   providers: [PokemonService]
 })
