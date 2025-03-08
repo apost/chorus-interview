9f5e1b55de3f9de61d144a7bf3ed3dee68944c0a
Andrew Post <andrewpost@gmail.com>
2025-03-07 17:17:47 -0800

    Trying ecosystem updates to reduce port collisions
    I noticed inspecting the pm2 logs that the inspectors were conflicting on port 9229
    This resulted in EADDRINUSE: address already in use :::9229 errors, which was presumably suppressing other logging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,27 @@ module.exports = {
       name: 'ui',
       script: 'pnpm pokemon-ui:serve',
       instance: 1,
+      env: {
+        NODE_ENV: 'development',
+        NODE_OPTIONS: '--inspect=0.0.0.0:9229', // Use a specific port for the inspector
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_OPTIONS: '--inspect=0.0.0.0:9230', // Use a different port for production
+      },
     },
     {
       name: 'server',
       script: 'pnpm pokemon-user-backend:serve',
       instance: 1,
+      env: {
+        NODE_ENV: 'development',
+        NODE_OPTIONS: '--inspect=0.0.0.0:9231', // Use a different port for the inspector
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_OPTIONS: '--inspect=0.0.0.0:9232', // Use a different port for production
+      },
     },
   ],
 };
