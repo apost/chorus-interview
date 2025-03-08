186d6fa557396b2839a82396a60119cfc8434a49
Andrew Post <andrewpost@gmail.com>
2025-03-07 20:14:08 -0800

    Replace hard-coded pokemon with dynamic query results mapping
    Uses react-query library to handle querying and caching responses
    and generally improve front-end dev quality of life
    observability wrappers around pending queries make error handling easier
    
    Uses the pokemon entity display_id to ensure actual db structure isn't reflected in front-end testing assumptions

--- packages/pokemon-ui/src/app/teamSelectionView.tsx
+++ packages/pokemon-ui/src/app/teamSelectionView.tsx
@@ -1,5 +1,9 @@
 import { css } from '@emotion/react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+import { PokemonDto } from '@dto/pokemon.dto';
 
 const containerStyle = css`
   display: flex;
@@ -41,16 +45,30 @@ const backButtonStyle = css`
   }
 `;
 
+const fetchPokemon = async (): Promise<PokemonDto[]> => {
+  const response = await axios.get('/api/pokemon');
+  return response.data;
+};
+
 function TeamSelectionView(){
   const navigate = useNavigate();
   let { team } = useParams();
   //  let data = useFakeDataLibrary(`/api/v2/teams/${team}`);
 
+  const { data: pokemonList, error, isLoading } = useQuery<PokemonDto[]>({
+    queryKey: ['pokemon'],
+    queryFn: fetchPokemon,
+  });
 
   const handlePokemonClick = (profile: string) => {
     console.log(`Selected pokemon: ${profile}`);
     // To-Do - Add the selected pokemon to the team
   };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading Pokémon</div>;
+
+  console.log('pokemonList - ' + pokemonList);
 
   return (
     <>
@@ -60,15 +78,16 @@ function TeamSelectionView(){
         </button>
         </h1>
         <div css={containerStyle} data-testid='selectable pokemon'>
-        <button data-testid='pokemon-1' css={buttonStyle} onClick={() => handlePokemonClick('Bulbasaur')}>
-            Bulbasaur
-        </button>
-        <button data-testid='pokemon-2' css={buttonStyle} onClick={() => handlePokemonClick('Ivysaur')}>
-            Ivysaur
-        </button>
-        <button data-testid='pokemon-3' css={buttonStyle} onClick={() => handlePokemonClick('Venasaur')}>
-            Venasaur
-        </button>
+          {pokemonList?.map((pokemon: { name: string, display_id: number }, index: number) => (
+            <button
+              key={index}
+              data-testid={`pokemon-${pokemon.display_id}`}
+              css={buttonStyle}
+              onClick={() => handlePokemonClick(pokemon.name)}
+            >
+              {pokemon.name}
+            </button>
+          ))}
         </div>
     </>
   );
