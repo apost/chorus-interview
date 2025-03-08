412a147d1e3255616910fceb8f743809159c50e7
Andrew Post <andrewpost@gmail.com>
2025-03-07 20:11:56 -0800

    Create a Pokemon DTO
    An aliased dto between front and backend allows shared types
    without creating a hard import dependency between the modules
    
    Use the dto as the promised type response from the pokemon controller
    I considered defining it as a class with default values instead of an interface
    Then I could have undefined pokemon be MissingNo.

---
+++ packages/pokemon-user-backend/src/modules/database/dto/pokemon.dto.ts
@@ -0,0 +1,5 @@
+export interface PokemonDto {
+    prototype_id: number;
+    display_id: number;
+    name: string;
+  }
--- packages/pokemon-user-backend/src/modules/pokemon/pokemon.controller.ts
+++ packages/pokemon-user-backend/src/modules/pokemon/pokemon.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonPrototype } from '../database/entities/PokemonPrototype';
+import { PokemonDto } from '../database/dto';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Get()
-  async findAll(): Promise<PokemonPrototype[]>{
+  async findAll(): Promise<PokemonDto[]>{
     return this.pokemonService.findAll();
   }
 }
