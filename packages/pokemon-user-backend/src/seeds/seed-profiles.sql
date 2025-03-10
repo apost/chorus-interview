ba8ef8e2d866adc540c3be0f3885eafbd402d849
Andrew Post <andrewpost@gmail.com>
2025-03-10 10:41:32 -0700

    Update seed upserts to also update key sequences

--- packages/pokemon-user-backend/src/seeds/seed-profiles.sql
+++ packages/pokemon-user-backend/src/seeds/seed-profiles.sql
@@ -4,3 +4,6 @@ INSERT INTO profile (profile_id, username, created_at) VALUES
 ON CONFLICT (profile_id) DO UPDATE SET
 username = EXCLUDED.username,
 created_at = EXCLUDED.created_at;
+
+-- Update the sequence for profile_id
+SELECT setval('profile_profile_id_seq', (SELECT MAX(profile_id) FROM profile));
--- packages/pokemon-user-backend/src/seeds/seed-teams.sql
+++ packages/pokemon-user-backend/src/seeds/seed-teams.sql
@@ -3,3 +3,6 @@ INSERT INTO team (team_id, profile_id) VALUES
 (2, 2)
 ON CONFLICT (team_id) DO UPDATE SET
 profile_id = EXCLUDED.profile_id;
+
+-- Update the sequence for team_id
+SELECT setval('team_team_id_seq', (SELECT MAX(team_id) FROM team));
