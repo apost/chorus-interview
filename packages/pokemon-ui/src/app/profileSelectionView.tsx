301f5f29b1bbdd27c8017da5012314babb5adfbf
Andrew Post <andrewpost@gmail.com>
2025-03-10 09:27:19 -0700

    Dynamically show existing profiles on profile selection view

--- packages/pokemon-ui/src/app/profileSelectionView.tsx
+++ packages/pokemon-ui/src/app/profileSelectionView.tsx
@@ -1,4 +1,6 @@
+import { ProfileDto } from '@dto/profile.dto';
 import { css } from '@emotion/react';
+import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 const containerStyle = css`
@@ -17,6 +19,7 @@ const buttonStyle = css`
   margin: 10px;
   font-size: 18px;
   cursor: pointer;
+  text-transform: capitalize;
   transition: background-color 0.3s;
 
   &:hover {
@@ -24,26 +27,39 @@ const buttonStyle = css`
   }
 `;
 
+const fetchProfiles = async (): Promise<ProfileDto[]> => {
+  const response = await fetch('/api/profiles');
+  return response.json();
+}
+
 const ProfileSelectionView: React.FC = () => {
   const navigate = useNavigate();
+  const { data: profiles, error, isLoading } = useQuery<ProfileDto[]>({
+    queryKey: ['profiles'],
+    queryFn: fetchProfiles,
+  });
 
   const handleProfileClick = (profile: string) => {
     console.log(`Selected profile: ${profile}`);
-    navigate('/team/'+profile); 
+    navigate('/team/' + profile);
     // Navigate to the team view with placeholder team id for that profile
   };
 
   return (
     <>
-        <h1 data-testid="greeting">Profile Selection</h1>
-        <div css={containerStyle} data-testid="profile-selection-container">
-          <button css={buttonStyle} onClick={() => handleProfileClick('red')} data-testid="profile-button-red">
-              Red Profile
+      <h1 data-testid="greeting">Profile Selection</h1>
+      <div css={containerStyle} data-testid="profile-selection-container">
+        {profiles?.map((profile) => (
+          <button
+            key={profile.profile_id}
+            css={buttonStyle}
+            onClick={() => handleProfileClick(profile.username)}
+            data-testid={`profile-button-${profile.username.toLowerCase()}`}
+          >
+            {profile.username.toLocaleLowerCase()} Profile
           </button>
-          <button css={buttonStyle} onClick={() => handleProfileClick('blue')} data-testid="profile-button-blue">
-              Blue Profile
-          </button>
-        </div>
+        ))}
+      </div>
     </>
   );
 };
